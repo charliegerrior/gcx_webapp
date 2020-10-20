ce564166b8944de1907270d82b6f7566c8dd5950
Charles Gerrior <charlie@luisella.local>
2020-10-20 17:43:36 -0400

    initial commit

---
+++ app/models.py
@@ -0,0 +1,26 @@
+from app import db
+from datetime import datetime
+
+class Submission(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    reddit_id = db.Column(db.String(7), unique=True)
+    reddit_link = db.Column(db.String(100), unique=True)
+    title = db.Column(db.Text, index=True)
+    created_at = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    offer = db.relationship("Offer", back_populates="submission")
+    status = db.Column(db.String(10), index=True)
+
+    def __repr__(self):
+      return '<{}>'.format(self.title)
+
+class Offer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    type = db.Column(db.String(8), index=True)
+    symbol = db.Column(db.String(8), index=True)
+    qty = db.Column(db.Integer)
+    price = db.Column(db.Float)
+    submission_id = db.Column(db.String(7), db.ForeignKey('submission.reddit_id'))
+    submission = db.relationship("Submission", back_populates="offer", uselist=False)
+
+    def __repr__(self):
+      return '<{} {} - ${} x {}>'.format(self.type, self.symbol, self.price, self.qty)
---
+++ app/routes.py
@@ -0,0 +1,60 @@
+from flask import render_template
+from app import app, db
+from app.models import Submission, Offer
+from datetime import datetime,timedelta
+
+@app.route('/')
+@app.route('/index')
+def index():
+    query = db.session.query(Offer.symbol.distinct().label("symbol"))
+    symbols = [row.symbol for row in query.all()]
+    gcs = []
+    for symbol in symbols:
+        try:
+            bid = db.session.query(Offer).filter_by(symbol=symbol, type="bid").join(Submission).order_by(Submission.created_at.desc()).first()
+            ask = db.session.query(Offer).filter_by(symbol=symbol, type="ask").join(Submission).order_by(Submission.created_at.desc()).first()
+            price = (bid.price * bid.qty + ask.price * ask.qty)/(bid.qty + ask.qty)
+            #vol = Offer.query.filter_by(symbol=symbol).count()
+            vol =  db.session.query(Offer).filter_by(symbol=symbol).join(Submission).filter(Submission.created_at.between(datetime.utcnow() - timedelta(hours=24), datetime.utcnow())).count()
+            gc = {"symbol": symbol, "bid": bid.price, "ask": ask.price, "volume": vol, "price": price}
+            gcs.append(gc)
+        except:
+            pass
+
+    return render_template('index.html', title='Home', gcs = gcs)
+
+@app.route('/<symbol>')
+def gc(symbol):
+    symbol = symbol.upper()
+    #bid = db.session.query(Offer).filter_by(symbol=symbol, type="bid").join(Submission).order_by(Submission.created_at.desc()).first()
+    recentBids = db.session.query(Offer).filter_by(symbol=symbol, type="bid").join(Submission).order_by(Submission.created_at.desc()).limit(10).all()
+    recentAsks = db.session.query(Offer).filter_by(symbol=symbol, type="ask").join(Submission).order_by(Submission.created_at.desc()).limit(10).all()
+    #ask = db.session.query(Offer).filter_by(symbol=symbol, type="ask").join(Submission).order_by(Submission.created_at.desc()).first()
+    latestBid = recentBids[0]
+    latestAsk = recentAsks[0]
+    price = (latestBid.price * latestBid.qty + latestAsk.price * latestAsk.qty)/(latestBid.qty + latestAsk.qty)
+    #vol = Offer.query.filter_by(symbol=symbol).count()
+    vol = db.session.query(Offer).filter_by(symbol=symbol).join(Submission).filter(Submission.created_at.between(datetime.utcnow() - timedelta(hours=24), datetime.utcnow())).count()
+
+    return render_template('gc.html', gc = gc, bid = latestBid, ask = latestAsk, symbol = symbol, vol = vol, price = price, bids = recentBids, asks=recentAsks)
+
+@app.route('/<symbol>/bids')
+def bids(symbol):
+    symbol = symbol.upper()
+    bids = db.session.query(Offer).filter_by(symbol=symbol, type="bid").join(Submission).order_by(Submission.created_at.desc()).limit(10).all()
+    return render_template('bids.html', symbol = symbol, bids = bids)
+
+@app.route('/<symbol>/asks')
+def asks(symbol):
+    symbol = symbol.upper()
+    asks = db.session.query(Offer).filter_by(symbol=symbol, type="ask").join(Submission).order_by(Submission.created_at.desc()).limit(10).all()
+    
+    return render_template('asks.html', symbol = symbol, asks = asks)
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
+
+@app.route('/legal')
+def legal():
+    return render_template('legal.html', title='Legal')
